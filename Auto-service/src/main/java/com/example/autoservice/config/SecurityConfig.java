d539f17259271512e70f718fc39dabb9421cae19
Denismgtu <92043052+Denismgtu@users.noreply.github.com>
2024-12-06 18:59:36 +0300

    H2

--- Auto-service/src/main/java/com/example/autoservice/config/SecurityConfig.java
+++ Auto-service/src/main/java/com/example/autoservice/config/SecurityConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
@@ -20,29 +18,24 @@ public class SecurityConfig {
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (webSecurity) ->
-                webSecurity.ignoring().requestMatchers("/api/register");
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/", "/api/**", "/css/**", "/js/**").permitAll() // Доступ к публичным ресурсам
+                .csrf(csrf -> csrf.disable()) // Отключаем CSRF для консоли H2
+                .headers(headers -> headers.frameOptions().disable()) // Разрешаем использование фреймов для H2 Console
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/api/**", "/css/**", "/js/**", "/h2-console/**").permitAll() // Разрешаем доступ к консоли H2
                         .anyRequest().authenticated()) // Остальные URL требуют авторизации
-                .formLogin((form) -> form
+                .formLogin(form -> form
                         .loginPage("/") // Главная страница выполняет роль выбора входа
                         .loginProcessingUrl("/login") // URL для обработки логина
                         .defaultSuccessUrl("/home", true) // После успешного входа перенаправляем на /home
                         .permitAll())
-                .logout((logout) -> logout
+                .logout(logout -> logout
                         .logoutUrl("/logout") // URL для выхода
                         .logoutSuccessUrl("/") // После выхода перенаправляем на главную
                         .invalidateHttpSession(true) // Очистка сессии
                         .clearAuthentication(true) // Очистка аутентификации
                         .permitAll());
-
 
         return http.build();
     }
